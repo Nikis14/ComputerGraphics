f34d28d94d6cb53e4518a8cc8eb691f79321a07e
SokolovMichail <sokolovmisha44@gmail.com>
2019-09-14 16:14:42 +0300

    Something like an initial commit. Created a file interface and forms

---
+++ Task1Graphics/Task1Graphics/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1Graphics
+{
+    public partial class Form1 : Form
+    {
+        Image source;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string ExecuteFileDialog()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "c:\\";
+            ofd.Filter = "Image Files(*.PNG;*.JPG)|*.PNG;*.JPG" ;
+            ofd.RestoreDirectory = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                 return ofd.FileName;
+            }
+            else
+            {
+                return "NONINIT";
+            }
+   
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(ExecuteFileDialog());
+        }
+    }
+}
