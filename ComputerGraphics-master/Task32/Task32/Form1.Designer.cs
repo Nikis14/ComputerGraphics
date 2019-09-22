096c36659d6d91860cfcb775c993f0973a9b2797
SokolovMichail <sokolovmisha44@gmail.com>
2019-09-22 16:03:46 +0300

    Half-done task 2 of lab 3

---
+++ ComputerGraphics-master/Task32/Task32/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Task32
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 82);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1182, 600);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1182, 47);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(84, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "DrawBorder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 682);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ComputerGraphics-master/Task32/Task32/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task32
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image;
+        bool allow_click=false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            image = new Bitmap(ExecuteFileDialog());
+            this.allow_click = true;
+            pictureBox1.Image = image;
+        }
+
+
+        private string ExecuteFileDialog()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = "c:\\";
+            ofd.Filter = "Image Files(*.PNG;*.JPG)|*.PNG;*.JPG";
+            ofd.RestoreDirectory = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                return ofd.FileName;
+            }
+            else
+            {
+                return "NONINIT";
+            }
+
+        }
+
+        private Tuple<int,int> get_pixel_near(int xsource,int ysource,int direction)
+        {
+            switch (direction)
+            {
+                case 0:
+                    return Tuple.Create(xsource - 1, ysource - 1);
+                case 1:
+                    return Tuple.Create(xsource, ysource - 1);
+                case 2:
+                    return Tuple.Create(xsource+1, ysource - 1);
+                case 3:
+                    return Tuple.Create(xsource+1, ysource);
+                case 4:
+                    return Tuple.Create(xsource+1, ysource + 1);
+                case 5:
+                    return Tuple.Create(xsource, ysource + 1);
+                case 6:
+                    return Tuple.Create(xsource-1, ysource + 1);
+                case 7:
+                    return Tuple.Create(xsource-1, ysource);
+                default:
+                    return Tuple.Create(xsource, ysource - 1);
+            }
+        }
+
+        //012
+        //7x3
+        //654
+        private List<string> find_border(int x,int y)
+        {
+           
+            List<String> points=new List<string>();
+            HashSet<string> points_visited= new HashSet<string>();
+            bool first_time = true;
+            Color c = image.GetPixel(x, y);
+            int direction = 5;
+            int curr_x = x;
+            int curr_y = y;
+            points_visited.Add(x.ToString() + "," + y.ToString());
+            while (true)
+            {
+                pictureBox1.Image = image;
+                if(first_time)
+                {
+                    first_time = false;
+                    for (int i = 0; i < 8; i++)
+                    {
+                        Tuple<int, int> point = get_pixel_near(curr_x, curr_y, Math.Abs((direction-i) % 8));
+                        String str = point.Item1.ToString() + "," + point.Item2.ToString();
+                        if (image.GetPixel(point.Item1,point.Item2) == c)
+                        {
+                            if (!points_visited.Contains(str))
+                            {
+                                direction = i;
+                                curr_x = point.Item1;
+                                curr_y = point.Item2;
+                                image.SetPixel(curr_x, curr_y, Color.Red);
+                                points_visited.Add(str);
+                                points.Add(str);
+                                break;
+                            }
+                            else return points;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < 8; i++)
+                    {
+                        Tuple<int, int> point = get_pixel_near(curr_x, curr_y, Math.Abs((direction + i) % 8));
+                        String str = point.Item1.ToString() + "," + point.Item2.ToString();
+                        if (image.GetPixel(point.Item1, point.Item2) == c)
+                        {
+                            if (!points_visited.Contains(str))
+                            {
+                                direction = i;
+                                curr_x = point.Item1;
+                                curr_y = point.Item2;
+                                image.SetPixel(curr_x, curr_y, Color.Red);
+                                points_visited.Add(str);
+                                points.Add(str);
+                                break;
+                            }
+                            else return points;
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void Picturebox_Mouse_Click(object sender,MouseEventArgs e)
+        {
+            List<string> t = find_border(448, 42);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HashSet<string> border = new HashSet<string>();
+            List<string> t = find_border(447, 41);
+        }
+    }
+}
